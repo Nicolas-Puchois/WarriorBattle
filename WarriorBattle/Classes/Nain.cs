a0f6cac27cb2a522819dd6ddec7dd94966fc5c62
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-07-11 13:38:15 +0200

    Feat: add Nain class file

---
+++ WarriorBattle/Classes/Nain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorBattle.Classes
+{
+    internal class Nain : Guerrier
+    {
+        private int _ptBouclier;
+
+
+        public int Bouclier { get => _ptBouclier; set => _ptBouclier = value; }
+        public Nain (string nom, int pointsDeVie, int nbAttaques ,int bouclier):base(nom, pointsDeVie, nbAttaques) 
+        { 
+            Bouclier = bouclier;
+        }
+    }
+}
