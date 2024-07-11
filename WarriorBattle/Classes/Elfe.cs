4f8f7a73ca6b82fed8a33f9610aaf6b0e8d35473
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-07-11 13:38:03 +0200

    Feat: add Elfe class file

---
+++ WarriorBattle/Classes/Elfe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarriorBattle.Classes
+{
+    internal class Elfe : Guerrier
+    {
+        private int _attassure = 2;
+
+        public int AttaqueAssure { 
+            get { return _attassure; } 
+        }
+
+        public Elfe(string nom, int pointsDeVie, int nbAttaques) : base (nom, pointsDeVie, nbAttaques)
+        {
+        
+        }
+            
+            
+     }
+}
